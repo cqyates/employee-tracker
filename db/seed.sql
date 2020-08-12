0fc96046c79ddbd109fd444ac5694209e56580b4
Corey <corey@oak.works>
2020-08-12 15:55:16 -0400

    set up prompt and switch

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO department(name) VALUES ("accouting"), ("sales"), ("engineering"), ("legal");
+INSERT INTO role(title, salary, department_id) VALUES ("Accountant", 95000, 1), ("Account Manager", 65000, 1), ("Sales Rep", 75000, 2), ("Sales Lead", 125000, 2), ("Junior Developer", 48000, 3), ("Senior Software Developer", 175000, 3 ), ("Legal Assistant", 35000, 4), ("Lawyer", 175000, 4);
+INSERT INTO employee(first_name, last_name, role_id) VALUES ("Fred", "Willard", 1), ("Catherine", "O'Hara", 2), ("Christopher", "Guest", 3), ("Parker", "Posey",4)
+	
---
+++ server.js
@@ -0,0 +1,39 @@
+const { prompt } = require("inquirer")
+
+function promptUser() {
+  prompt([{
+    type: "list",
+    name: "direction",
+    message: "What would you like to do?",
+    choices: ["View All Employees", "View All Departments", "View All Roles", "Exit"]
+  }]).then(answer => {
+    switch(answer.direction) {
+      case "View All Employees":
+        View_All_Employees()
+        break;
+      case "View All Departments":
+        View_All_Departments()
+        break;
+        case "View All Roles":
+          View_All_Roles()
+      default:
+        goodBye()
+    } 
+  })
+}
+function View_All_Employees() {
+  console.log("Here is your full roster of employees");
+  promptUser()
+}
+function View_All_Departments() {
+  console.log("Here are your active departments");
+  promptUser()
+}
+function View_All_Roles() {
+  console.log("Here are the current roles for your organization");
+  promptUser()
+}
+function goodBye() {
+  console.log("Thanks for using the Employee Tracker, We keep track!")
+}
+promptUser()
